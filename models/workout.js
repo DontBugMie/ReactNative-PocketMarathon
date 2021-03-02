e02b990d6dfbc750589f26d3d58e686cda5a2427
Maya Wright <mayagwright20@hotmail.com>
2021-03-02 14:26:04 +0000

    2:25 2nd march working all well. intervaals are being added to the creaete workour screen and the summary screen

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,7 @@
-let titleDate = new Date();
-titleDefault = titleDate.toString();
-let titleDefaultArr = titleDefault.split(" ");
-let titleDafaultString =  titleDefaultArr[2] + "/" + titleDefaultArr[1] + "/" + titleDefaultArr[3] + " "  + titleDefaultArr[4];
+// let titleDate = new Date();
+// titleDefault = titleDate.toString();
+// let titleDefaultArr = titleDefault.split(" ");
+// let titleDafaultString =  titleDefaultArr[2] + "/" + titleDefaultArr[1] + "/" + titleDefaultArr[3] + " "  + titleDefaultArr[4];
 
 
 
